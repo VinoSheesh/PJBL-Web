9221613634b49803c7d4a36992e899a02410f357
VinoSheesh <orvinoray@gmail.com>
2024-11-28 01:19:13 +0700

    updated

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
 const hamburger = document.getElementById('hamburger-menu');
 const navMenu = document.getElementById('nav-menu');
+const counterContainer = document.querySelectorAll(".counters");
 
+counterContainer.forEach(container => {
+  const counterItems = container.querySelectorAll(".counter");
+  const counters = container.querySelectorAll("span");
+  let activated = false;
+
+  window.addEventListener("scroll", () => {
+    if (
+      scrollY > container.offsetTop - container.offsetHeight - 200 &&
+      activated === false
+    ) {
+      counterItems.forEach((item, i) => {
+        item.style.animation = "fadeUp 600ms ease-in-out forwards";
+        item.style.animationDelay = 0.2 * i + "s";
+      });
+
+      counters.forEach(counter => {
+        counter.innerText = 0;
+        let count = 0;
+
+        function updateCount() {
+          const target = parseInt(counter.dataset.count);
+          const speed = parseInt(counter.dataset.speed);
+
+          if (count < target) {
+            count++;
+            counter.innerText = count;
+            setTimeout(updateCount, speed);
+          } else {
+            counter.innerText = target;
+          }
+        }
+
+        updateCount();
+        activated = true;
+      });
+    } else if(
+      scrollY < container.offsetTop - container.offsetHeight - 500 || scrollY === 0
+      && activated === true
+    ) {
+      counters.forEach(counter => counter.innerText = 0);
+
+      counterItems.forEach(item => item.style.animation = "none");
+
+      activated = false;
+    }
+  });
+});
 hamburger.addEventListener('click', () => {
   if (navMenu.classList.contains('active')) {
    
